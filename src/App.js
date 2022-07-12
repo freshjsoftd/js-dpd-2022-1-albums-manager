ff241f2786923aae8c0b639c3233f88aa15d6b4b
yan <freshjs.oftd@gmail.com>
2022-07-12 12:11:10 +0300

    Add some styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {
 	NavLink,
 	// Link,
 	Switch,
-  Redirect,
+	Redirect,
 } from 'react-router-dom';
 import Albums from './components/Albums/Albums';
 import Users from './components/Users/Users';
@@ -13,33 +13,38 @@ import Users from './components/Users/Users';
 function App() {
 	return (
 		<Router>
-      <div className="header">
-        <ul>
-          <li>
-            <NavLink to='/albums'>Albums</NavLink>
-          </li>
-          <li>
-            <NavLink to='/users' activeClassName='selected'>Users</NavLink>
-          </li>
-          <li>
-            <NavLink to='/' activeClassName='selected'>Home</NavLink>
-          </li>
-        </ul>
-      </div>
-			<Switch>
-				<Route path='/albums' component={Albums}>
-					{/* <Albums /> */}
-				</Route>
-				<Route path='/users'>
-					<Users />
-				</Route>
-				<Route path='/' exact>
-					Home
-				</Route>
-				<Route path='*'>
-					<Redirect to='/users'></Redirect>
-				</Route>
-			</Switch>
+			<div className='container'>
+				<ul>
+					<li>
+						<NavLink to='/albums'>Albums</NavLink>
+					</li>
+					<li>
+						<NavLink to='/users' activeClassName='selected'>
+							Users
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/' activeClassName='selected'>
+							Home
+						</NavLink>
+					</li>
+				</ul>
+
+				<Switch>
+					<Route path='/albums' component={Albums}>
+						{/* <Albums /> */}
+					</Route>
+					<Route path='/users'>
+						<Users />
+					</Route>
+					<Route path='/' exact>
+						Home
+					</Route>
+					<Route path='*'>
+						<Redirect to='/users'></Redirect>
+					</Route>
+				</Switch>
+			</div>
 		</Router>
 	);
 }
